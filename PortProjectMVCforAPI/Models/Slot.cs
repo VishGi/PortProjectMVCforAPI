7693f19be2f135823d0f48c5a8ea18cc44a092ca
vishnusu <vishnusu@LAPTOP-SQ73UQ96>
2022-07-22 17:20:54 +0530

    Add project files.

---
+++ PortProjectMVCforAPI/Models/Slot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortProjectMVCforAPI.Models
+{
+    public class Slot
+    {
+        [Key]
+        public int ID { get; set; }
+        public int SlotId { get; set; }
+
+        public int RUID { get; set; }
+
+        public int SLUserID { get; set; }
+
+        public int Status { get; set; }
+        public DateTime Sdate { get; set; }
+
+        public DateTime Edate { get; set; }
+
+        public int Cost { get; set; }
+    }
+}
